d07e608ba89d70fb4be193efefe387093868f67c
Duan Liu <liuduan8@gmail.com>
2016-02-10 08:24:41 -0600

    For the moment, ./Local_PC_testing_files/Susan_helped-2.html on the local PC is submitting the post request, and ./Windows_server/ajaxll.php on the Amazon Web Services is responding the post request. Susan Liu (LIU Zhonghong 刘中红) helped a lot to figure out.

--- Windows_server/ajaxll.php
+++ Windows_server/ajaxll.php
@@ -1,4 +1,14 @@
 <?php
+/* This file is used to test the .post() function as a remote server file
+It was located at http://ec2-52-24-231-219.us-west-2.compute.amazonaws.com/ajaxll.php.
+The local PC testing file was ./Local_PC_testing_files/Susan_helped-2.html 
+It works very well.
+This line in remote PHP file is required to allow post access in Amazon Web Services:
+header("Access-Control-Allow-Origin: *");
+
+Susan Liu (LIU Zhonghong) helped to figure out.
+-- Duan Liu, 2016-Feb */
+
 header("Access-Control-Allow-Origin: *");
 
 $term = strip_tags(substr($_POST['search_term'],0, 100));
--- Windows_server/php-info.php
+++ Windows_server/php-info.php
@@ -1,4 +1,50 @@
 <?php
+if(isset($["Data"]))
+{
+	$method=$_SERVER['REQUEST_METHOD'];
+	$data="";
+ 	if($method=="POST"){
+		$data=$_POST["Data"];
+
+		$fakeData=new FakeData();
+		$fakeData->Data="Hi remote friend, you tried to POST some mock data: *"+data+"* to me.";
+		$fakeData->Time=new DateTime("now");
+		}
+ 		elseif($method=="GET")
+			{
+			$fakeData=new FakeData();
+			$fakeData->Data="Hi remote friend, you tried to passed me data: *"+data+"* through HTTP GET.";
+			$fakeData->Time=new DateTime("now");
+			}
+ 		else
+			{
+			RaiseError();
+			}
+
+	header('Content-type: application/json');
+	$jsonStr= json_encode($fakeData);
+ 	echo($jsonStr);
+	}
+	else
+		{
+		RaiseError();
+		}
+
+function RaiseError()
+{
+http_send_status(405);
+header("Status: 405 Method Not Allowed");
+}
+
+/*Classes definition*/
+class FakeData
+{
+public $Data;
+public $Time;
+}
+?>
+
+<?php
  
 $term = strip_tags(substr($_POST['search_term'],0, 100));
 //$term = mysql_escape_string($term);
