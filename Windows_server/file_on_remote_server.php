5a81757161164b58e11988a549f758dcf6832ba5
Duan Liu <liuduan8@gmail.com>
2016-02-11 15:56:24 -0600

    The json receiving is working with plain text transfer. After that, signs like %22%2C are replaced by ", So it will fit into json_decode() in PHP.

--- Windows_server/file_on_remote_server.php
+++ Windows_server/file_on_remote_server.php
@@ -9,104 +9,14 @@ header("Access-Control-Allow-Origin: *");
 Susan Liu (LIU Zhonghong) helped to figure out.
 -- Duan Liu, 2016-Feb */
 header("Access-Control-Allow-Origin: *");
-echo "Hello, here is from server:..";
-echo '<br>=================================*';
-$from_input = file_get_contents('php://input'); 
-
-echo '<br>working: var_dump($from_input); ';
-var_dump($from_input);
+echo "Hello, world!    ";
 
 
-echo '<br>working: echo $from_input: ';
-echo $from_input;
-$from_input = str_replace("%22", '"', $from_input);
-echo $from_input;
-
-$from_input = str_replace("%2C", ',', $from_input);
-echo $from_input;
-
-$from_input = str_replace("%7D", '}', $from_input);
-echo $from_input;
-
-$from_input = str_replace("%7B", '{', $from_input);
-echo $from_input;
-
-$from_input = str_replace("%3A", ':', $from_input);
-echo $from_input;
-
-$from_input = substr($from_input, 5, (strlen($from_input)-5));
-echo $from_input;
-
-$obj_from_input = json_decode($from_input, true, $depth = 512);
-echo '<br>working: var_dump($obj_from_input): ------- ';
-var_dump($obj_from_input);
-
-echo '<br>working: print_r($obj_from_input): ';
-$obj_from_input = (array)$obj_from_input;
-print_r($obj_from_input);
-
-echo '$obj_from_input["submittion-2"]: ';
-echo $obj_from_input["submittion-2"];
-
-echo "  <br>========================.*". "========================.*.";
-
-$obj_from_input_2 = json_decode('{"foo-bar": 12345,"foo-bar-2": 678}', true, $depth = 512);
-
-echo '<br>working: var_dump($obj_from_input_2); ';
-var_dump($obj_from_input_2);
-print_r($obj_from_input_2);
-
-
-echo "  <br>========================.*".$_SERVER['REQUEST_METHOD']. "========================.*.";
-
-if ($_SERVER['REQUEST_METHOD'] == 'POST')
-{
-  $data = json_decode(file_get_contents("php://input"), false, $depth = 512);
-  var_dump($data);
-  print_r($data);
-}
-/*
-echo "========================.*.**";
-$json = '{"a":1,"b":2,"c":3,"d":4,"e":5}';
-echo $json;
-
-var_dump(json_decode($json));
-var_dump(json_decode($json, true));
-
-*/
-
-
-
-
-
-echo $_POST;
-
-
-
-echo '<br>Works: var_dump($_POST):';
-var_dump($_POST);
-$equal_post = $_POST;
-echo '<br>Works: var_dump($equal_post): ';
-var_dump($equal_post);
-
-$obj = json_decode($equal_post, false, $depth = 512);
-echo '<br>not working: var_dump($obj): ';
-var_dump($obj); 
-echo '<br>not working: print_r($obj) = ';
+// echo $HTTP_RAW_POST_DATA; 
+// $json = file_get_contents("php://input"); 
+$obj = json_decode($_POST, false, $depth = 512);
+echo '<br>print_r($obj) =';
 print_r($obj);
-
-
-
-echo '<br>============================.. working: var_dump($obj_from_input); ';
-var_dump($obj_from_input);
-$data = (array)$obj;
-
-echo '<br>not working: print_r($obj_from_input) = ';
-print_r($obj_from_input);
-
-
-
-echo '<br>=================================';
 $data = (array)$obj;
 echo '<br>$data["submittion"] = '. $data["submittion"];
 echo '<br>';
@@ -115,7 +25,7 @@ echo '<br>';
 $string = var_dump($_POST);
 echo '$String ='. $string;
 
-
+var_dump($_POST);
 
 // $string = "Should be an array.";
 
